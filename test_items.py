ce5682d4c9ca1112a7aa53ce0d6f64bbfb01fa91
Aleksei Berdnikov <Aleksei240398@gmail.com>
2022-09-07 19:36:04 +0400

    rename method in test_items.py

--- test_items.py
+++ test_items.py
@@ -2,11 +2,11 @@ import pytest
 import time
 from selenium.webdriver.common.by import By
 
-def test_items(browser):
+def test_the_presence_of_a_button(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.implicitly_wait(5)
     button = len(browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket"))
     assert button == 1
-    time.sleep(10)
+    time.sleep(3)
     browser.quit()
